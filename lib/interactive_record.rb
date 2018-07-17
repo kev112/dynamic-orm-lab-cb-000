fe9220525fa92dc68e002a176fac66c8a7150f76
Tao <werewaswas@live.com>
2018-07-17 16:36:56 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,49 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
-  
+  # def initialize(options={})
+  #   options.each do |property, value|
+  #     self.send("#{property}=", value)
+  #   end
+  # end
+
+  def initialize(options={})
+    options.each do
+      |property, value| self.send(`#{property}=`, value)
+    end
+  end
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql =  "pragma table_info('#{self.table_name}')"
+    info_hash = DB[:conn].execute(sql)
+    column_names = []
+    info_hash.each do
+      |column| column_names << column['name']
+
+    end
+
+    column_names.compact
+  end
+
+  def table_name_for_insert
+    self.class.table_name.join(', ')
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(', ')
+  end
+
+  def find_by(attribute_hash)
+    sql = "SELECT * FROM WHERE  = #{attribute_hash.keys.first} "
+    
+  end
+
 end
